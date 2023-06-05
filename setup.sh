98a943e14585c671debeb5f2a34ba1852d584803
DevinDiehl <77985632+DevinDiehl@users.noreply.github.com>
2023-06-05 11:18:07 -0400

    Update setup.sh
    Update the shell file to run with the new version of PHaRLAP 4.5.1

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ echo "Example - /home/devindiehl/Pylap_project"
 read x
 echo ${x}
 
-export PHARLAP_HOME="${x}/pharlap_4.5.0"
-echo "export PHARLAP_HOME=${x}/pharlap_4.5.0" >> ~/.bashrc
+export PHARLAP_HOME="${x}/pharlap_4.5.1"
+echo "export PHARLAP_HOME=${x}/pharlap_4.5.1" >> ~/.bashrc
 
 if [[ -d "${x}/PyLap" ]] ;
 then
@@ -30,8 +30,8 @@ fi
 export LD_LIBRARY="${x}/l_comp_lib_2020.4.304_comp.for_redist/compilers_and_libraries_2020.4.304/linux/compiler/lib/intel64_lin"
 echo "export LD_LIBRARY=${x}/l_comp_lib_2020.4.304_comp.for_redist/compilers_and_libraries_2020.4.304/linux/compiler/lib/intel64_lin" >> ~/.bashrc
 
-export DIR_MODELS_REF_DAT="${x}/pharlap_4.5.0/dat"
-echo "export DIR_MODELS_REF_DAT=${x}/pharlap_4.5.0/dat">> ~/.bashrc
+export DIR_MODELS_REF_DAT="${x}/pharlap_4.5.1/dat"
+echo "export DIR_MODELS_REF_DAT=${x}/pharlap_4.5.1/dat">> ~/.bashrc
 
 source /home/$USER/bin/compilervars.sh intel64
 echo "source /home/$USER/bin/compilervars.sh intel64" >>  ~/.bashrc
@@ -39,13 +39,13 @@ echo "source /home/$USER/bin/compilervars.sh intel64" >>  ~/.bashrc
 
 # echo "python3 setup.py install --user" >> ~/.bashrc
 
-# export PHARLAP_HOME="/home/devindiehl/Pylap_project/PHARLAP/pharlap_4.5.0"
+# export PHARLAP_HOME="/home/devindiehl/Pylap_project/PHARLAP/pharlap_4.5.1"
 
 # export PYTHONPATH="/home/devindiehl/Pylap_project/PyLap"
 
 # export LD_LIBRARY="/home/devindiehl/Pylap_project/l_comp_lib_2020.4.304_comp.for_redist/compilers_and_libraries_2020.4.304/linux/compiler/lib/intel64_lin" 
 
-# export DIR_MODELS_REF_DAT="/home/devindiehl/Pylap_project/PHARLAP/pharlap_4.5.0/dat"
+# export DIR_MODELS_REF_DAT="/home/devindiehl/Pylap_project/PHARLAP/pharlap_4.5.1/dat"
 
 
 sudo apt-get install python3-tk python3-pil python3-pil.imagetk libqt5gui5 python3-pyqt5 
